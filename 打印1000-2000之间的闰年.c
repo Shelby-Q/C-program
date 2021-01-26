ce67692ac060886308e396fa9d7f932258b26cf0
Qian <77046330+Shelby-Q@users.noreply.github.com>
2021-01-26 17:03:20 +0800

    Update 打印1000-2000之间的闰年.c

--- 打印1000-2000之间的闰年.c
+++ 打印1000-2000之间的闰年.c
@@ -4,20 +4,21 @@ int main()
 {
 	int year = 0;
 	int count = 0;
+	printf("1000至2000是的闰年有：\n");
 	for (year =1000;year <= 2000; year++)
 	{
-		
-		if (year % 4 == 0 && year % 1000 != 0)
+		if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
 		{
 			printf("%d  ", year);
 			count++;
 		}
-		else if (year % 400 == 0)
+		/*else if (year % 400 == 0)
 		{
 			printf("%d  ", year);
 			count++;
-		}
+		}*/
 	}
 	printf("\n一共有：%d个\n",count);
+	system("pause");
 	return 0;
 }
