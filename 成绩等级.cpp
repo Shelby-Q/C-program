dcb3e04f3f864109ad0780cbf6e5c94eb49d3db2
Qian <77046330+Shelby-Q@users.noreply.github.com>
2021-01-28 19:39:34 +0800

    Add files via upload

---
+++ 成绩等级.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+	int score;
+	printf("ÇëÊäÈëÄúµÄ³É¼¨£º");
+	scanf("%d",&score);
+	if(score >= 90)
+	{
+		printf("A");
+	}
+	else if(80 <= score < 90)
+	{
+		printf("B");
+	}
+	else if(70 <= score < 80)
+	{
+		printf("C");
+	}
+	else if(60 <= score < 70)
+	{
+		printf("D");
+	}
+	else
+	{
+		printf("E");
+	}
+
+	return 0;
+}
