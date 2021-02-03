b9859eba1a1661886da6305affad8b673788f484
Qian <77046330+Shelby-Q@users.noreply.github.com>
2021-02-03 22:03:09 +0800

    Create memset函数 初始化函数.c

---
+++ 函数/memset函数 初始化函数.c
@@ -0,0 +1,19 @@
+//memset函数 初始化函数   memory ：记忆  set：设置
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	//void * memset ( void * ptr, int value, size_t num );
+	/*
+	ptr: 指向要填充的内存块的指针
+	value:用这个值转换填充内存块
+	num：要设置为value的字节数。size_t是无符号整数类型。
+	**/
+	char arr[] = "hello world";
+	memset(arr, '3' , 5);
+	printf("%s\n", arr);
+	return 0; 
+}
