f9f6d0af3e5bf5fc357fb8355f5e01543fd11bc5
Qian <77046330+Shelby-Q@users.noreply.github.com>
2021-02-05 15:25:14 +0800

    Create 闰年 is_leap_year.c

---
+++ 函数/闰年 is_leap_year.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+//is_leap_year 是闰年吗？
+int is_leap_year(int y)
+{
+	if ((y % 4 == 0 && y % 100 == 0) || (y % 400 == 0))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	
+	}
+}
+int main()
+{
+	int year = 0;
+	printf("闰年有：\n");
+	for (year = 100;year <= 2000;year++)
+	{
+		//判断是否为闰年
+		if (is_leap_year(year) == 1)
+		{
+			printf("%d  ", year);
+		}
+	}
+	return 0;
+}
