1b7a89571428dead0d455c18e994d11ed7af62fa
Qian <77046330+Shelby-Q@users.noreply.github.com>
2021-02-21 15:50:02 +0800

    Create 菲波那切数列.c

---
+++ 函数/菲波那切数列.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+//菲波那切数列
+//1 1 2 3 5 8 13 21 34 55.....
+
+int Fib(int n)
+{
+	int a = 1;
+	int b = 1;
+	int c = 1;
+	while (n>2)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		n--;
+	}
+	return c;
+}
+
+int main()
+{
+	int n = 0;
+	int ret = 0;
+	scanf("%d", &n);
+	ret = Fib(n);
+	printf("ret = %d\n", ret);
+	return 0;
+}
