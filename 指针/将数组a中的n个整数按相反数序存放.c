4e75241541e6cfbef7858d2dabe43747c011197a
Qian <77046330+Shelby-Q@users.noreply.github.com>
2021-03-31 22:12:20 +0800

    Update 将数组a中的n个整数按相反数序存放.c

--- 指针/将数组a中的n个整数按相反数序存放.c
+++ 指针/将数组a中的n个整数按相反数序存放.c
@@ -1,6 +1,6 @@
 //将数组a中的n个整数按相反数序存放
 
-void inv(int x[], int n)
+void inv(int x[], int n)							//形参x是数组名				
 {
 	int temp, i, j, m = (n - 1) / 2;
 	for (i=0;i<=m;i++)
@@ -8,7 +8,7 @@ void inv(int x[], int n)
 		j = n - 1 - i;
 		temp = x[i];
 		x[i] = x[j];
-		x[j] = temp;
+		x[j] = temp;                                    		//将x[i]与x[j]交换
 	}
 	return 0;
 }
@@ -18,15 +18,16 @@ int main()
 	printf("The original arry:\n");
 	for (i = 0; i < 10; i++)
 	{
-		printf("%d", a[i]);
+		printf("%d", a[i]);						//输出未交换时的函数值
 	}
 	printf("\n");
-	inv(a, 10);
+	inv(a, 10);								//调用inv函数  进行交换								  
 	printf("The arrary has been inverted: \n");
 	for (i = 0; i < 10; i++)
 	{
-		printf("%d", a[i]);
+		printf("%d", a[i]);						//输出交换后的数组的个元素的值
 	}
 	printf("\n");
 	return 0;
-	}
+	
+}
