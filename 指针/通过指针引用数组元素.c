e8a6d1ccd37599d672a0b259d2449de1000c959b
Qian <77046330+Shelby-Q@users.noreply.github.com>
2021-04-03 00:27:43 +0800

    Update 通过指针引用数组元素.c

--- 指针/通过指针引用数组元素.c
+++ 指针/通过指针引用数组元素.c
@@ -62,4 +62,17 @@ int main()
 	printf("\n");
 	return 0;
 }
+//数组名表示的是数组首元素的地址
+#include <stdio.h>
+int main()
+{
+    int arr[] = {1,2,3,4,5,6,7,8,9,0};
+    int *p = arr; //指针存放数组首元素的地址
+    int sz = sizeof(arr)/sizeof(arr[0]);     
+    for(i=0; i<sz; i++)
+    {
+        printf("&arr[%d] = %p   <====> p+%d = %p\n", i, &arr[i], i, p+i);     
+    }
+    return 0; 
+}
 
