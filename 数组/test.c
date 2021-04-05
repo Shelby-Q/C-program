c30ed719cac2f9aa65233f801c3fd7df270f09ee
Qian <77046330+Shelby-Q@users.noreply.github.com>
2021-04-05 21:23:05 +0800

    Update test.c

--- 数组/test.c
+++ 数组/test.c
@@ -32,6 +32,22 @@ void Reverse(int arr[],int sz)
 		left++;
 		right--;
 	}
+	/*
+	交换方法二
+void Reverse(int x[], int n)
+{
+	int temp, i, j, m = (n - 1) / 2;
+	for(i=0;i<=m;i++)
+	{
+		j = n - 1 - i;
+		temp = x[i];
+		x[i] = x[j];
+		x[j] = temp;
+	}
+	return;
+}
+
+	*/
 }
 
 int main()
